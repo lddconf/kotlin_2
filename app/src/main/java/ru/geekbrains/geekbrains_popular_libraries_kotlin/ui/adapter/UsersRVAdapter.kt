828ab367007a0b8f1c3b478ee67f5e06efefce7e
priputin <v-priputin@yandex.ru>
2021-04-01 16:59:32 +0300

    Плановая фиксация

--- app/src/main/java/ru/geekbrains/geekbrains_popular_libraries_kotlin/ui/adapter/UsersRVAdapter.kt
+++ app/src/main/java/ru/geekbrains/geekbrains_popular_libraries_kotlin/ui/adapter/UsersRVAdapter.kt
@@ -9,34 +9,34 @@ import ru.geekbrains.geekbrains_popular_libraries_kotlin.mvp.model.image.IImageL
 import ru.geekbrains.geekbrains_popular_libraries_kotlin.mvp.presenter.list.IUsersListPresenter
 import ru.geekbrains.geekbrains_popular_libraries_kotlin.mvp.view.list.IUserItemView
 
-class UsersRVAdapter(val presenter: IUsersListPresenter<UsersRVAdapter.ViewHolder>, val imageLoader: IImageLoader<ImageView>  ) :
-    RecyclerView.Adapter<UsersRVAdapter.ViewHolder>() {
+class UsersRVAdapter(val presenter: IUsersListPresenter<UsersRVAdapter.ViewHolder>, val imageLoader: IImageLoader<ImageView>) :
+        RecyclerView.Adapter<UsersRVAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder =
-        ViewHolder(
-            ItemUserBinding.inflate(
-                LayoutInflater.from(parent.context),
-                parent,
-                false
-            )
-        ).apply {
-            itemView.setOnClickListener { presenter.itemClickListener?.invoke(this) }
-        }
+            ViewHolder(
+                    ItemUserBinding.inflate(
+                            LayoutInflater.from(parent.context),
+                            parent,
+                            false
+                    )
+            ).apply {
+                itemView.setOnClickListener { presenter.itemClickListener?.invoke(this) }
+            }
 
     override fun getItemCount() = presenter.getCount()
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) =
-        presenter.bindView(holder.apply { pos = position })
+            presenter.bindView(holder.apply { pos = position })
 
     inner class ViewHolder(val vb: ItemUserBinding) : RecyclerView.ViewHolder(vb.root),
-        IUserItemView {
+            IUserItemView {
         override var pos = -1
 
         override fun setLogin(text: String) = with(vb) {
             tvLogin.text = text
         }
 
-        override fun loadAvatarUrl(url: String) = with(vb){
+        override fun loadAvatarUrl(url: String) = with(vb) {
             imageLoader.load(url, tvAvatar)
         }
     }
